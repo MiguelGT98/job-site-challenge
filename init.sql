993b148344980eab80c0529c0c8bcd32b0b64e18
Miguel Gallardo <jgallardo9814@gmail.com>
2021-12-14 19:22:24 -0600

    Better setup for the entire app

---
+++ init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS challenge;
+
+CREATE TABLE `users` (
+    `id` char(36) NOT NULL,
+    `email` varchar(255) DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `password` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+CREATE TABLE `jobs` (
+    `id` char(36) NOT NULL,
+    `title` varchar(255) DEFAULT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `created_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+CREATE TABLE `applications` (
+    `id` char(36) NOT NULL,
+    `job_id` char(36) NOT NULL,
+    `user_id` char(36) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `applications_job_id_foreign` (`job_id`),
+    KEY `applications_user_id_foreign` (`user_id`),
+    CONSTRAINT `applications_job_id_foreign` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`),
+    CONSTRAINT `applications_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
